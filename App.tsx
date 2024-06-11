e728968df02867f017740ab7e5933fa42a8a604a
Edmilson Soares <esbnet@gmail.com>
2024-06-11 20:38:08 -0300

    feat: :sparkles: instaled and configured OneSignal

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar } from "react-native";
+import { OneSignal } from "react-native-onesignal";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { Loading } from "./src/components/Loading";
+import { THEME } from "./src/theme";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+
+OneSignal.initialize("6072b751-a548-4ce1-878a-38a0797de19b");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
