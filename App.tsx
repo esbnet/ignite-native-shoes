588ffa0cbd6d22c7dd5189790d38c029530689a7
Edmilson Soares <esbnet@gmail.com>
2024-06-11 23:30:56 -0300

    feat: :sparkles: intall and config OneSignal Push Message

--- App.tsx
+++ App.tsx
@@ -14,7 +14,10 @@ import { THEME } from "./src/theme";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
 
-OneSignal.initialize("6072b751-a548-4ce1-878a-38a0797de19b");
+const FCM_ID = "6072b751-a548-4ce1-878a-38a0797de19b";
+
+OneSignal.initialize(FCM_ID);
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
